1da3ffb093ec1f38b1bfdbc367259424fff92343
sergei.budnikov <sergei.budnikov@smartseeds.ru>
2024-02-28 15:26:12 +0300

    Add project files.

---
+++ Func/DateTimeHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Func
+{
+    public static class DateTimeHelper
+    {
+        private static Func<DateTime> GetNow = () => DateTime.Now;
+        private static Func<DateTime> GetNowUtc = () => DateTime.UtcNow;
+
+        public static DateTime Now()
+        {
+            return GetNow.Invoke();
+        }
+
+        public static DateTime UtcNow()
+        {
+            return GetNowUtc.Invoke();
+        }
+
+        public static void OverrideNow(Func<DateTime> newNow)
+        {
+            GetNow = newNow;
+        }
+        public static void OverrideNowUtc(Func<DateTime> newNow)
+        {
+            GetNowUtc = newNow;
+        }
+    }
+
+}
+
+
---
+++ Func/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Func;
+
+//var date = DateTime.Now;
+DateTimeHelper.OverrideNow(() => DateTime.Today.AddHours(10).AddMinutes(DateTime.Now.Minute).AddSeconds(DateTime.Now.Second));
+DateTimeHelper.OverrideNowUtc(() => DateTime.Today.AddHours(7).AddMinutes(DateTime.Now.Minute).AddSeconds(DateTime.Now.Second));
+var date = DateTimeHelper.Now();
+
+var date2 = DateTimeHelper.UtcNow().AddHours(3);
+
+Console.WriteLine("Hello, World!");
